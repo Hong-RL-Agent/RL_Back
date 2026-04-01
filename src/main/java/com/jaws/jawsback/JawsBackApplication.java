f6b6ad596938a22a3b8784425f25d18699f6fc00
sinung02 <smileshinwoo@gmail.com>
2026-04-01 18:47:01 +0900

    Initial Commit

---
+++ src/main/java/com/jaws/jawsback/JawsBackApplication.java
@@ -0,0 +1,13 @@
+package com.jaws.jawsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JawsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JawsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaws/jawsback/JawsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaws.jawsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JawsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
